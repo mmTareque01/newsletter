7a28980eb75074d29c9939853797ac1f2c341a0c
Muhimenul Tareque <mmtareque@yahoo.com>
2025-05-14 16:58:10 +0600

    working code

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -26,7 +26,7 @@ export function errorHandler(
   const message = error.message || "Internal Server Error";
 
   if (error instanceof CustomError && error.statusCode in response) {
-    response[error.statusCode || "ER400"](res, error.details, error.message);
+    response[ "ER400"](res, error.details, error.message);
   }
 
   response.ER500(res, message);
