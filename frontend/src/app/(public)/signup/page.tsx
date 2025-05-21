047f562652f1a2ad8f6e7efc715e1ede38ba3e0a
Md Muhimenul Tareque <mmtareque@yahoo.com>
2025-05-21 09:09:28 +0600

    added dynamic code generator and upgraded signup and signin form . added implementation using dynamic ui generator

--- frontend/src/app/(public)/signup/page.tsx
+++ frontend/src/app/(public)/signup/page.tsx
@@ -1,76 +1,13 @@
 "use client";
 import Form from "@/components/form/Form";
 import GenerateUI from "@/components/GenerateUI";
+import OrBorder from "@/components/OrBorder";
 import SocialLogin from "@/components/SocialLogin";
-import { UIComponent } from "@/types/generateUI";
-import Link from "next/link";
-
-const fields = [
-  {
-    name: "firstName",
-    label: "First Name",
-    type: "text",
-    required: true,
-    placeholder: "Enter your first name",
-    validation: { required: "First name is required" },
-  },
-  {
-    name: "lastName",
-    label: "Last Name",
-    type: "text",
-    required: true,
-    placeholder: "Enter your last name",
-    validation: { required: "Last name is required" },
-  },
-  {
-    name: "email",
-    label: "Email",
-    type: "email",
-    required: true,
-    placeholder: "Enter your email",
-    validation: {
-      required: "Email is required",
-      pattern: {
-        value: /^\S+@\S+\.\S+$/,
-        message: "Enter a valid email",
-      },
-    },
-  },
-  {
-    name: "password",
-    label: "Password",
-    type: "password",
-    required: true,
-    placeholder: "Enter your password",
-    showPasswordToggle: true,
-    validation: {
-      required: "Password is required",
-      minLength: {
-        value: 6,
-        message: "Password must be at least 6 characters",
-      },
-    },
-  },
-  {
-    name: "confirmPassword",
-    label: "Confirm Password",
-    type: "password",
-    required: true,
-    placeholder: "Confirm your password",
-    showPasswordToggle: true,
-    validation: {
-      required: "Please confirm your password",
-      validate: (value: string, formValues: any) =>
-        value === formValues.password || "Passwords do not match",
-    },
-  },
-];
-
-const components: UIComponent[] = [
-  { component: 'h1', className: 'mb-2 font-semibold text-gray-800 text-title-sm sm:text-title-md', children: 'Sign In' },
-  { component: 'p', className:'text-sm text-gray-500', children: 'Enter your email and password to sign in!' },
-//   { component: MyCustomComponent, title: 'Custom Component' }
-];
+import {
+  SignUpFormFields,
+  SignUpTitle,
+  SignUptoSignIn,
+} from "@/constants/auth";
 
 export default function SignUpForm() {
   const handleSubmit = (data: Record<string, any>) => {
@@ -87,38 +24,22 @@ export default function SignUpForm() {
         {/* <GoBack/> */}
         <div className="flex flex-col justify-center flex-1 w-full max-w-md mx-auto">
           <div>
-            
             <GenerateUI
-              UIComponents={components}
-              wrapperComponent={'div'}
+              UIComponents={SignUpTitle}
+              wrapperComponent={"div"}
               wrapperClassName="mb-5 sm:mb-8"
             />
             <div>
               <SocialLogin />
-              <div className="relative py-3 sm:py-5">
-                <div className="absolute inset-0 flex items-center">
-                  <div className="w-full border-t border-gray-200 "></div>
-                </div>
-                <div className="relative flex justify-center text-sm">
-                  <span className="p-2 text-gray-400 bg-white  sm:px-5 sm:py-2">
-                    Or
-                  </span>
-                </div>
-              </div>
+              <OrBorder />
 
-              <Form fields={fields} onSubmit={handleSubmit} />
+              <Form fields={SignUpFormFields} onSubmit={handleSubmit} />
 
-              <div className="mt-5">
-                <p className="text-sm font-normal text-center text-gray-700  sm:text-start">
-                  Already have an account? {""}
-                  <Link
-                    href="/signin"
-                    className="text-brand-500 hover:text-brand-600 "
-                  >
-                    Sign In
-                  </Link>
-                </p>
-              </div>
+              <GenerateUI
+                UIComponents={SignUptoSignIn}
+                wrapperComponent={"div"}
+                wrapperClassName="my-1 flex items-center justify-center gap-1"
+              />
             </div>
           </div>
         </div>
