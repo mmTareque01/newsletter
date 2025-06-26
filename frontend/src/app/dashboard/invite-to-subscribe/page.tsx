266314608d21ee990dca6a69248719e51b4a712f
Md Muhimenul Tareque <mmtareque@yahoo.com>
2025-06-26 18:08:32 +0600

    added invite to subscribe ui

--- frontend/src/app/dashboard/invite-to-subscribe/page.tsx
+++ frontend/src/app/dashboard/invite-to-subscribe/page.tsx
@@ -1,10 +1,11 @@
 import ImportSubscribers from '@/pages/ImportSubscribers.page'
+import EmailComposeForm from '@/pages/InviteToSubscribe'
 import React from 'react'
 
 export default function page() {
     return (
         <>
-            <ImportSubscribers />
+            <EmailComposeForm />
         </>
     )
 }
